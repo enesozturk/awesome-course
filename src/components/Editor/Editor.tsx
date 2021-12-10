895d1e07bf42053e9e12713e37aea3409512e86c
Enes Ozturk <enesozturk.d@gmail.com>
2021-12-11 00:27:17 +0300

    refactor: create lesson and chapter files structure and update editor comp

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -4,36 +4,8 @@ import MonacoEditor from "@monaco-editor/react";
 
 import type { EditorProps, Page } from "./Editor.props";
 
-const examplePageProps = {
-  pageName: "index.js",
-  body: `
-  import Head from "next/head";
-  import dynamic from "next/dynamic";
-  
-  import styles from "../styles/Home.module.css";
-  
-  const Editor = dynamic(() => import("../src/components/Editor"), {
-    ssr: false,
-  });
-  
-  export default function Home() {
-    return (
-      <div className="app">
-        <main className="flex-1 flex bg-white dark:bg-gray-800 text-gray-900 dark:text-white relative overflow-scroll">
-          <div className="flex-1 p-8 box-border overflow-hidden">
-            <Editor />
-          </div>
-        </main>
-      </div>
-    );
-  }
-  `,
-};
-
-export default function Editor({ pages = [examplePageProps] }: EditorProps) {
-  const [activePage, setActivePage] = useState<Page | undefined>(
-    examplePageProps
-  );
+export default function Editor({ pages }: EditorProps) {
+  const [activePage, setActivePage] = useState<Page | undefined>(pages[0]);
 
   const editorRef = React.useRef(null);
 
