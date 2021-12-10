c06332c3e05fd217b1614ec21680cc8c15e37f88
Enes Ozturk <enesozturk.d@gmail.com>
2021-12-11 00:27:29 +0300

    chore: add link to home page to navigate chapter

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
-import dynamic from "next/dynamic";
-
-const Editor = dynamic(() => import("../src/components/Editor"), {
-  ssr: false,
-});
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="app">
       <main className="flex-1 flex bg-white dark:bg-gray-800 text-gray-900 dark:text-white relative overflow-scroll">
+        <div className="flex-1 p-8 pb-0 box-border overflow-scroll markdown"></div>
         <div className="flex-1 p-8 box-border overflow-hidden">
-          <Editor />
+          <Link href="lesson/1/chapter/13">go course</Link>
         </div>
       </main>
     </div>
