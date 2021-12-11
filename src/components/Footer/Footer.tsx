f29a842285697ad48882c9500022261c59f7a4a1
Enes Ozturk <enesozturk.d@gmail.com>
2021-12-11 22:52:27 +0300

    refactor: update footer styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,6 +15,8 @@ export default function Footer({
   showAnswer,
   setShowAnswer,
 }: FooterProps) {
+  const isLastChapter = currentChapterIndex === chaptersLength - 1;
+
   return (
     <footer className="flex justify-between w-full px-8 py-4 box-border bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 text-gray-900 dark:text-white">
       <div className="flex-1 flex justify-start">
@@ -33,7 +35,7 @@ export default function Footer({
           onClick={() => {
             setShowAnswer(!showAnswer);
           }}
-          className="p-2 box-border bg-blue-100 hover:bg-blue-200 dark:bg-blue-800 dark:hover:bg-blue-800 dark:bg-opacity-40 dark:hover:bg-opacity-50 text-blue-500 dark:text-blue-400 flex items-center justify-center rounded-md cursor-pointer"
+          className="p-2 box-border bg-amber-100 hover:bg-amber-200 dark:bg-amber-800 dark:hover:bg-amber-800 dark:bg-opacity-40 dark:hover:bg-opacity-50 text-amber-500 dark:text-amber-400 flex items-center justify-center rounded-md cursor-pointer"
         >
           {showAnswer ? "Hide Answer" : "Show Answer"}
         </button>
@@ -46,35 +48,37 @@ export default function Footer({
             }`}
           >
             <a
-              className={`p-2 box-border flex items-center justify-center rounded-md cursor-pointer
+              className={`p-2 box-border flex items-center justify-center rounded-md
                 ${
                   !prevChapter
-                    ? "text-gray-300"
-                    : "bg-black dark:bg-white bg-opacity-5 dark:bg-opacity-5 hover:bg-opacity-10 dark:hover:bg-opacity-10"
+                    ? "text-gray-500 cursor-default"
+                    : "bg-black dark:bg-white bg-opacity-5 dark:bg-opacity-5 hover:bg-opacity-10 dark:hover:bg-opacity-10 cursor-pointer"
                 }
                 `}
             >
               Previous Chapter
             </a>
           </Link>
-          <div className="p-2 mx-4 box-border flex items-center justify-center rounded-md cursor-pointer">
+          <div className="p-2 mx-4 box-border flex items-center justify-center rounded-md cursor-default">
             {`${currentChapterIndex + 1} / ${chaptersLength}`}
           </div>
           <Link
-            href={`/lesson/${currentLessonId}/chapter/${
-              nextChapter?.id || currentChapterId
+            href={`/lesson/${currentLessonId}/${
+              isLastChapter
+                ? "completed"
+                : `chapter/${nextChapter?.id || currentChapterId}`
             }`}
           >
             <a
               className={`p-2 box-border flex items-center justify-center rounded-md cursor-pointer
               ${
-                !nextChapter
-                  ? "text-gray-300"
+                isLastChapter
+                  ? "bg-green-200 hover: bg-green-400 dark:bg-green-800 dark:hover:bg-green-900 text-green-500 dark:text-green-400"
                   : "bg-black dark:bg-white bg-opacity-5 dark:bg-opacity-5 hover:bg-opacity-10 dark:hover:bg-opacity-10"
               }
               `}
             >
-              Next Chapter
+              {isLastChapter ? "Complete Lesson" : "Next Chapter"}
             </a>
           </Link>
         </div>
