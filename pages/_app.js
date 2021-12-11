7bf2693eab199c395828db812a98970f394a41ec
Enes Ozturk <enesozturk.d@gmail.com>
2021-12-11 12:01:03 +0300

    feat: add toast component and wrap app with editor provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import { Toaster } from "react-hot-toast";
+
+import { EditorProvider } from "../src/context/providers";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <EditorProvider>
+        <Component {...pageProps} />
+        <Toaster containerClassName="mb-20" />
+      </EditorProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
