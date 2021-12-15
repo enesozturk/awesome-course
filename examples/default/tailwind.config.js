c02bc450906bbfc869701828a375a57138de0250
Ali Emir Şen <senaliemir@gmail.com>
2021-12-16 00:59:10 +0300

    chore: migrate to tailwindcss 3 (#3)

--- examples/default/tailwind.config.js
+++ examples/default/tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  mode: "jit",
-  purge: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "media",
   theme: {
     screens: {
       xs: "414px",
